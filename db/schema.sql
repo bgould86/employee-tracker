2a9520b12f71f765e761665e10bfcb0fd03ae0c8
bgould86 <bgould86@gmail.com>
2022-03-10 17:44:21 -0600

    schema is set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,30 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
-CREATE TABLE departments
+USE employee_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL,
+    FOREIGN KEY (dept_id)
+    REFERENCES departments(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE
+);
