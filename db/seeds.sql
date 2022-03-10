5f8d706ce17098ab732818f3cf3b5dac285f29f2
bgould86 <bgould86@gmail.com>
2022-03-10 17:59:37 -0600

    seeds are set up

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO departments (dept_name)
+VALUES ("Administration"),
+       ("Research and Development"),
+       ("Marketing and Sales"),
+       ("Human Resources"),
+       ("Accounting and Finance"),
+       ("Customer Service");
+
+INSERT INTO roles (title, salary, dept_id)
+VALUES ("Manager", 100000.00, 1),
+       ("Associate", 80000.00, 2),
+       ("Sales Lead", 80000.00, 3),
+       ("Waste of Money", 90000.00, 4),
+       ("Accountant", 90000.00, 5),
+       ("Customer Rep", 40000.00, 6);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Brett", "Gould", 1, 1),
+        ("Nick", "Gamblin", 2, 1),
+        ("Andrew", "Lynch", 3, 1),
+        ("Derb", "Snerdly", 4, 1),
+        ("Money", "Handleman", 5, 1),
+        ("Gibby", "Gruber", 6, 1);
+      
